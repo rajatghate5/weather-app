2dd2345f435b6cd4f286d7f4677dfafa037e30a1
rajatghate5 <rajatghate5@gmail.com>
2023-12-02 13:18:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=pune&units=metric&appid=15bffc924b5db95377544e0f3be341c2"
+    )
+      .on("data", (chunk) => {
+        const objData = JSON.parse(chunk);
+        const arrData = [objData];        
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server is running at http://127.0.0.1:8000')
+});
